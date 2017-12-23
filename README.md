3a0e181db54f67c46f10423b037fac540dd5837c
bike <usemodj@gmail.com>
2017-12-23 23:19:20 +0900

    setup `babel-preset-env` for ES6 modules

--- README.md
+++ README.md
@@ -7,4 +7,43 @@ Once you've got the code, install the dependencies by navigating to the director
 ```
 npm install
 
-``` 
+```
+
+## Upgrading to `babel-preset-env`
+<https://babeljs.io/env>
+
+### Install `babel-preset-env`
+```
+# Remove `babel-preset-es2015`:
+$ npm uninstall babel-preset-es2015 --save-dev
+
+# Install `babel-preset-env`:
+$ yarn add --dev babel-preset-env
+```
+
+### Basic `.babelrc` change
+```
+{
+  "presets": ["env"]
+}
+```
+
+### Install other useful grunt plugins:
+```
+$ yarn add --dev jit-grunt time-grunt grunt-contrib-clean grunt-contrib-jshint
+```
+
+
+------------------------------------------
+
+## Build
+
+#### For development:
+```
+$ grunt buildDev
+```
+
+#### For production:
+```
+$ grunt buildProd
+```
--- gruntfile.js
+++ gruntfile.js
@@ -1,52 +1,126 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-    grunt.initConfig({
+  // Load grunt tasks automatically, when needed
+  require('jit-grunt')(grunt, {
+    browserify: 'grunt-browserify',
+  });
 
-        browserify: {
-            development: {
-                src: [
-                    "./js/main-home.js",
-                    "./js/main-products.js"
-                ],
-                dest: './dist/js/common.js',
-                options: {
-                    browserifyOptions: { debug: true },
-                    transform: [["babelify", { "presets": ["es2015"] }]],
-                    plugin: [
-                      ["factor-bundle", { outputs: [
-                            "./dist/js/main-home.js",
-                            "./dist/js/main-products.js"
-                        ] }]
-                    ],
-                    watch: true,
-                    keepAlive: true,
-                }
-            },
-            production: {
-                src: [
-                    "./js/main-home.js",
-                    "./js/main-products.js"
-                ],
-                dest: './dist/js/common.min.js',
-                options: {
-                    browserifyOptions: { debug: false },
-                    transform: [["babelify", { "presets": ["es2015"] }]],
-                    plugin: [
-                      ["factor-bundle", { outputs: [
-                            "./dist/js/main-home.min.js",
-                            "./dist/js/main-products.min.js"
-                        ] }],
-                      ["minifyify", { map: false }]
-                    ]
-                }
-            }
-        },
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    });
+
+  grunt.initConfig({
+
+    // Project settings
+    pkg: grunt.file.readJSON('package.json'),
+
+    app: {
+      // configurable paths
+      client: require('./package.json').appPath || '.',
+      server: 'server',
+      dist: 'dist'
+    },
+
+    browserify: {
+      development: {
+        src: [
+          "<%= app.client %>/js/main-home.js",
+          "<%= app.client %>/js/main-products.js"
+        ],
+        dest: '<%= app.server %>/js/common.js',
+        options: {
+          browserifyOptions: {
+            debug: true
+          },
+          transform: [
+            ["babelify", {
+              "presets": ["env"]
+            }]
+          ],
+          plugin: [
+            ["factor-bundle", {
+              outputs: [
+                "<%= app.server %>/js/main-home.js",
+                "<%= app.server %>/js/main-products.js"
+              ]
+            }]
+          ],
+          watch: true,
+          keepAlive: true,
+        }
+      },
+      production: {
+        src: [
+          "<%= app.client %>/js/main-home.js",
+          "<%= app.client %>/js/main-products.js"
+        ],
+        dest: './dist/js/common.min.js',
+        options: {
+          browserifyOptions: {
+            debug: false
+          },
+          transform: [
+            ["babelify", {
+              "presets": ["env"]
+            }]
+          ],
+          plugin: [
+            ["factor-bundle", {
+              outputs: [
+                "<%= app.dist %>/js/main-home.min.js",
+                "<%= app.dist %>/js/main-products.min.js"
+              ]
+            }],
+            ["minifyify", {
+              map: false
+            }]
+          ]
+        }
+      }
+    },
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '<%= app.dist %>/!(.git*|.openshift|Procfile)**'
+          ]
+        }]
+      },
+      server: '<%= app.server %>'
+    },
+
+  });
+
+  // grunt.loadNpmTasks('grunt-browserify'); // instead use `jit-grunt` plugin
+
+  grunt.registerTask("init", function(){
+    console.log('> app.dist = ', grunt.config('app.dist'));
+    console.log('> app.server = ', grunt.config.process('<%= app.server %>'));
     
-    grunt.loadNpmTasks('grunt-browserify');
+    let dist = grunt.config('app.dist');
+    let server = grunt.config('app.server');
+    // Like the Node.js `path.join` method,
+    if (!grunt.file.exists(dist)) {
+      // Works like `mkdir -p`. Create a directory along with any intermediate directories.
+      grunt.file.mkdir(dist);
+    }
 
-    grunt.registerTask("buildDev", ['browserify:development']);
-    grunt.registerTask("buildProd", ['browserify:production']);
+    if (!grunt.file.exists(server)) {
+      grunt.file.mkdir(server);
+    }
+  });
+
+  grunt.registerTask("buildDev", [
+    'clean:server',
+    'init',
+    'browserify:development'
+  ]);
+  grunt.registerTask("buildProd", [
+    'clean:dist',
+    'init',
+    'browserify:production'
+  ]);
 
 };
