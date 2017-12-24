f3d6d2230f33273e64056b394b9be8e979cc82ae
bike <usemodj@gmail.com>
2017-12-24 20:55:24 +0900

    update readme

--- README.md
+++ README.md
@@ -5,20 +5,65 @@ An example setup for ES6 modules with Browserify and Grunt. You can find the acc
 Once you've got the code, install the dependencies by navigating to the directory and running the following command:
 
 ```
-npm install
+$ yarn install
+# Or,
+$ npm install
 
 ```
 
 ## Upgrading to `babel-preset-env`
 <https://babeljs.io/env>
 
+Usage: `Yarn` package manager <https://yarnpkg.com/en/docs/cli/>
+```
+# help:
+$ yarn help
+$ yarn help add
+
+# `add` or `remove` <package  package  ... > :
+$ yarn add <packages ...> [--dev]
+$ yarn remove <packages ...>
+
+# install all dependencies for a project :
+$ yarn [install]
+```
+
+Usage: `npm` package manager
+```
+# help:
+$ npm help
+$ npm help install
+
+# `install` or `uninstall` <package  package  ...> :
+# Package will appear in your `dependencies`.
+# This is the `default`,
+$ npm install <packages ...>  [--save-prod]
+
+# Package will appear in your `devDependencies`,
+$ npm install <packages ...>  --save-dev
+
+# Package will be removed from your `dependencies`.
+$ npm uninstall <packages ...> --save
+
+# Package will be removed from your `devDependencies`.
+$ npm uninstall <packages ...> --save-dev
+
+# install all dependencies for a project :
+$ npm install
+```
+
+
 ### Install `babel-preset-env`
+prefer `yarn` to `npm`.
+
 ```
 # Remove `babel-preset-es2015`:
 $ npm uninstall babel-preset-es2015 --save-dev
 
 # Install `babel-preset-env`:
 $ yarn add --dev babel-preset-env
+# Or,
+$ npm install --save-dev babel-preset-env
 ```
 
 ### Basic `.babelrc` change
@@ -36,6 +81,9 @@ $ yarn add --dev jit-grunt time-grunt grunt-newer grunt-contrib-clean grunt-cont
 
 ### Install `eslint`:
  <https://eslint.org/docs/user-guide/getting-started>
+> `ESLint` is a tool for identifying and reporting on patterns found in `ECMAScript/JavaScript` code,
+> with the goal of making code more consistent and avoiding bugs.
+> In many ways, it is similar to `JSLint` and `JSHint`.
 ```
 $ yarn add --dev eslint  grunt-eslint
 ```
--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,17 @@
-module.exports = function(grunt) {
 
+module.exports = function (grunt) {
   // Load grunt tasks automatically, when needed
   require('jit-grunt')(grunt, {
     newer: 'grunt-newer',
     eslint: 'grunt-eslint',
-    browserify: 'grunt-browserify',
-  });
-
+    browserify: 'grunt-browserify'
+  })
   // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
-
+  require('time-grunt')(grunt)
 
   grunt.initConfig({
-
     // Project settings
     pkg: grunt.file.readJSON('package.json'),
-
     app: {
       // configurable paths
       client: require('./package.json').appPath || '.',
@@ -24,18 +20,19 @@ module.exports = function(grunt) {
     },
 
     eslint: {
-  		options: {
-  			configFile: '.eslintrc.json',
-  			// rulePaths: ['conf/rules']
-  		},
-  		// target: ['file.js']
-  	},
+      options: {
+        fix: true,
+        configFile: '.eslintrc.json'
+        // rulePaths: ['conf/rules']
+      }
+      // target: ['file.js']
+    },
 
     browserify: {
       development: {
         src: [
-          "<%= app.client %>/js/main-home.js",
-          "<%= app.client %>/js/main-products.js"
+          '<%= app.client %>/js/main-home.js',
+          '<%= app.client %>/js/main-products.js'
         ],
         dest: '<%= app.server %>/js/common.js',
         options: {
@@ -43,41 +40,39 @@ module.exports = function(grunt) {
             debug: true
           },
           transform: [
-            ["babelify", {
-              "presets": ["env"]
+            ['babelify', {
+              'presets': ['env']
             }]
           ],
           plugin: [
-            ["factor-bundle", {
+            ['factor-bundle', {
               // `opts.o` or `opts.outputs` should be an `array` that pairs up with the `files` array to specify
               // where each bundle output for each entry file(`src`) should be written.
               // The elements in `opts.o` can be `string` filenames, writable `streams`, or `functions`
               // that return a `string filename` or writable `stream`.
               outputs: [
-                "<%= app.server %>/js/main-home.js",
-                "<%= app.server %>/js/main-products.js"
+                '<%= app.server %>/js/main-home.js',
+                '<%= app.server %>/js/main-products.js'
               ],
               // `opts.threshold` can be a `number` or a function `opts.threshold(row, groups)`
               // where `row` is a `module-deps` object and `groups` is an array of bundles which depend on the row.
               // If the `threshold` function returns `true`, that `row` and all its dependencies will go to the `common` bundle.
               // If `false`, the `row` (but not its dependencies) will go to each bundle in `groups`.
-              threshold: function(row, groups) { // go to the `common` bundle file if threshold returns `true`
+              threshold: function (row, groups) { // go to the `common` bundle file if threshold returns `true`
                 // if (/.*a\.js$/.test(row.id)) return false;  // matching row file go to the bundles which depend on the `row`.
                 // if (/.*[z]\.js$/.test(row.id)) return true; // matching row file go to the `common` bundle(`common[.min].js`)
-                if (/.*a\.js$/.test(row.id)) return false;
-                if (/.*[z]\.js$/.test(row.id)) return true;
-                return this._defaultThreshold(row, groups);
-              },
+                return this._defaultThreshold(row, groups)
+              }
             }]
-          ],
+          ]
           // watch: true,
           // keepAlive: true,
         }
       },
       production: {
         src: [
-          "<%= app.client %>/js/main-home.js",
-          "<%= app.client %>/js/main-products.js"
+          '<%= app.client %>/js/main-home.js',
+          '<%= app.client %>/js/main-products.js'
         ],
         dest: './dist/js/common.min.js',
         options: {
@@ -85,18 +80,18 @@ module.exports = function(grunt) {
             debug: false
           },
           transform: [
-            ["babelify", {
-              "presets": ["env"]
+            ['babelify', {
+              'presets': ['env']
             }]
           ],
           plugin: [
-            ["factor-bundle", {
+            ['factor-bundle', {
               outputs: [
-                "<%= app.dist %>/js/main-home.min.js",
-                "<%= app.dist %>/js/main-products.min.js"
+                '<%= app.dist %>/js/main-home.min.js',
+                '<%= app.dist %>/js/main-products.min.js'
               ]
             }],
-            ["minifyify", {
+            ['minifyify', {
               map: false
             }]
           ]
@@ -114,48 +109,47 @@ module.exports = function(grunt) {
         }]
       },
       server: '<%= app.server %>'
-    },
+    }
 
-  });
+  })
 
   // grunt.loadNpmTasks('grunt-browserify'); // Instead, use `jit-grunt` plugin
 
-  grunt.registerTask("init", function() {
-    console.log('> app.client = ', grunt.config('app.client'));
-    console.log('> app.dist = ', grunt.config('app.dist'));
-    console.log('> app.server = ', grunt.config.process('<%= app.server %>'));
+  grunt.registerTask('init', function () {
+    console.log('> app.client = ', grunt.config('app.client'))
+    console.log('> app.dist = ', grunt.config('app.dist'))
+    console.log('> app.server = ', grunt.config.process('<%= app.server %>'))
 
-    let dist = grunt.config('app.dist');
-    let server = grunt.config('app.server');
+    let dist = grunt.config('app.dist')
+    let server = grunt.config('app.server')
     // Like the Node.js `path.join` method,
     if (!grunt.file.exists(dist)) {
       // Works like `mkdir -p`. Create a directory along with any intermediate directories.
-      grunt.file.mkdir(dist);
+      grunt.file.mkdir(dist)
     }
     if (!grunt.file.exists(server)) {
-      grunt.file.mkdir(server);
+      grunt.file.mkdir(server)
     }
-  });
+  })
 
   // build development
-  grunt.registerTask("buildDev", [
+  grunt.registerTask('buildDev', [
     'clean:server',
     'init',
     'browserify:development'
-  ]);
-  
+  ])
+
   // build production
-  grunt.registerTask("buildProd", [
+  grunt.registerTask('buildProd', [
     'clean:dist',
     'init',
     'browserify:production'
-  ]);
+  ])
 
   // build default
   grunt.registerTask('default', [
     'newer:eslint',
     'buildDev',
     'buildProd'
-  ]);
-
-};
+  ])
+}
